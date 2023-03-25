6a425fc65d44274c9ea93925f9375887d082cd34
licha8 <leecha@qq.com>
2023-03-25 09:55:31 +0800

    ‘20230325’

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 import App from './App.vue'
 import router from './router'
+
+import axios from 'axios'//引入axios
 
 import './assets/main.css'
 
 const app = createApp(App)
-
+app.config.globalProperties.$axios=axios
 app.use(createPinia())
 app.use(router)
-
+app.use(ElementPlus)
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,44 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <el-tree
+        :data="data"
+        :props="defaultProps"      
+        default-expand-all >
+        <template #default="{ node, data }">
+            <template v-if="data.type=='url'">
+                <el-link :href="data.url" type="primary" target="_blank">{{ node.label }}</el-link>
+            </template>
+            <template v-if="data.type!='url'">{{ node.label }}</template>
+        </template>
+    </el-tree>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+        data:[],
+        defaultProps:{
+            children: 'children',
+            label: 'name',
+        },
+    }
+  },
+  created() {
+    this.load()
+  },
+  methods: {
+    load(){   
+        fetch('./src/Bookmarks') 
+        .then(response => response.json())
+        .then(res => { 
+            this.data=res.roots.bookmark_bar.children
+        })
+    },
+  }   
+}
+</script>
+<style>
+.el-tree-node__content{
+    border-bottom: 1px solid #e3e3e3;
+}
+</style>
